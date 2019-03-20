2118bd16bb84c23574a82ed988eff9fbd2cea0fe
Joaquin <jtagle2@uc.cl>
2019-03-20 15:19:19 -0300

    First request, ugly but almost working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+
 class WelcomeController < ApplicationController
+
+  def index
+    uri = URI('https://swapi.co/api/films/')
+    req = Net::HTTP::get(uri)
+    @films = JSON.parse(req)['results']
+  end
+
 end
