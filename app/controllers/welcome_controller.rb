3904fc3d80a3943eb89c5f68d9fd790dc230b097
Joaquin <jtagle2@uc.cl>
2019-03-24 16:50:27 -0300

    Removed file uploaded by my brother

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    uri = URI('https://swapi.co/api/films/')
-    req = Net::HTTP::get(uri)
-    @films = JSON.parse(req)['results']
     @films = get_all('https://swapi.co/api/films/')
     @films.each do |film|
       film["uri"] = URI(film["url"]).path.sub("/api", "")
