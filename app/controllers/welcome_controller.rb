e5c5ea86f1a70dfa8266bce6773c58504edad8b2
Joaquin <jtagle2@uc.cl>
2019-03-24 23:18:08 -0300

    Added final changes including the search bar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,12 @@ class WelcomeController < ApplicationController
     @films = get_all('https://swapi.co/api/films/')
   end
 
+  def search
+    search_param = params['name']
+    @films = get_all('https://swapi.co/api/films/?search=' + search_param)
+    @starships = get_all('https://swapi.co/api/starships/?search=' + search_param)
+    @people = get_all('https://swapi.co/api/people/?search=' + search_param)
+    @planets = get_all('https://swapi.co/api/planets/?search=' + search_param)
+  end
+
 end
